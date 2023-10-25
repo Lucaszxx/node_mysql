ddd3685a560e5c4af56d2f06be0c6b37ff6947c3
Lucaszxx <lucasd476@gmail.com>
2023-10-24 22:09:38 -0300

    primeira conexão com mysql

---
+++ index.js
@@ -0,0 +1,47 @@
+const { response } = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql2");
+
+const app = express();
+
+//definindo handlebars como template engine
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// pasta de arquivos estáticos como CSS, imagens
+app.use(express.static("public"));
+
+//trabalhar com dados formato json
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json());
+
+//rotas
+app.get("/", (req, res) => {
+    response.render("home")
+})
+
+// Conexão com MYSQL
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodemysql",
+    port: 3307
+})
+
+conn.connect((error) => {
+    if(error) {
+        console.log(error)
+        return
+    }
+
+    console.log("Conectado ao MySQL!");
+    
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000");
+    })
+})
